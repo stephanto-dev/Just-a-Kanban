a6420af54dcce57b044fcd81a977753bca6e321e
Vinicius Stephanto <50119865+stephanto-dev@users.noreply.github.com>
2023-08-01 14:55:51 -0300

    Fixed for deploy

--- server/src/database/data-source.ts
+++ server/src/database/data-source.ts
@@ -1,13 +1,18 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
+import { Card } from "./models/Card";
+import { Status } from "./models/Status";
+import { User } from "./models/User";
 
+const modelsDir = process.env.API_START_ARG === 'dev' ? './src/database/models/**/*.ts' : 'dist/database/models/**/*.js';
+const migrationsDir = process.env.API_START_ARG === 'dev' ? './src/database/migrations/**/*.ts' : 'dist/database/migrations/**/*.js';
 
 export const AppDataSource = new DataSource({
     type: "sqlite",
     database: "database.sqlite",
     synchronize: true,
     logging: false,
-    entities: ["./src/database/models/**.ts"],
-    migrations: ["./src/database/migrations/**.ts"],
+    entities: [Card, Status, User],
+    migrations: [migrationsDir],
     subscribers: [],
 })
